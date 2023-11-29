49fc7db295905a20d92f90f8549506d5c88becd3
6Maryjos6 <joseaperdomo06@gmail.com>
2023-11-29 19:58:54 -0400

    resumen

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //alert("Hola este es mi Javascript");
+function enviarformulario(){
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    if (!nombre || !correo || !mensaje) {
+        alert("todos los campos son abligatorios");
+        return;
+
+    }
+    console.log("nombre", nombre);
+    console.log("correo",correo);
+    console.log("mensaje",mensaje);
+
+    document.getElementById("formulario").reset();
+}
